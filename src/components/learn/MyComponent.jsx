57763eaf38f9ab4840a272f539cbbde1887a8835
John Tran <phuocthanh03062004@gmail.com>
2026-02-26 01:16:23 +0700

    Cách sử dụng biến số với JSX, Cách dùng code JS trong React

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -5,11 +5,22 @@
 import "./styles.css"
 
 const MyComponent = () => {
+    const string = "Hello" // String
+    const number = 36 // Number
+    const bool = true // Boolean
+    const unde = undefined // undefined
+    const ull = null // Null
+    const arr = [1 , 2, 3] // Array
+    const obj = {
+        name: "John",
+        age: 22
+    } // Object
   return (
     <>
-      <h2>Hello React</h2>
+      <h2>{JSON.stringify(obj)} React</h2>
+      <div>{console.log("Hello Console")}</div>
       <div className="child">
-        <h5 style={{fontSize: "25px"}}>Child</h5>
+        <h5 style={{fontSize: "25px"}}>Child</h5> {/*Cái style ở đây có 2 ngoặc nhọn là vì cái bên ngoài dùng ngoặc nhọn để dùng JS cái thứ 2 là Object*/}
       </div>
     </>
   )
