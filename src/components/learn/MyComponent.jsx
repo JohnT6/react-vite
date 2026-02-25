e141cdb561833ee2b34ebe3853a8b9fd4e5d122b
John Tran <phuocthanh03062004@gmail.com>
2026-02-26 00:58:55 +0700

    Video 30: JSX, Fragment, CSS external, CSS Inline

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,17 @@
+// JSX: 1 parent
+// Fragment
+// CSS external
+// CSS Inline
+import "./styles.css"
+
 const MyComponent = () => {
   return (
-    <div>
+    <>
       <h2>Hello React</h2>
-    </div>
+      <div className="child">
+        <h5 style={{fontSize: "25px"}}>Child</h5>
+      </div>
+    </>
   )
 }
 
