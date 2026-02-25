f753999ad3579275bebf74e9b45ae4093fd277a9
John Tran <phuocthanh03062004@gmail.com>
2026-02-26 00:07:00 +0700

    Tạo Component đầu tiên

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const MyComponent = () => {
+  return (
+    <div>
+      <h2>Hello React</h2>
+    </div>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -17,6 +25,7 @@ function App() {
         </a>
       </div>
       <h1>Hello Word</h1>
+      <MyComponent/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
