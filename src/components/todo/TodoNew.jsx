f7f0135dfae1c148b4a7bedf30e4810c6143dd9b
John Tran <phuocthanh03062004@gmail.com>
2026-02-27 01:39:28 +0700

    #36. DOM Event. Cách sử dụng DOM Event trong React, cách sử dụng hàm và cách sử arrow function với hàm có tham số

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,10 +1,20 @@
 const TodoNew = ({ addNewFunction }) => {
     // Nhận function từ cha App.jsx và gọi ở TodoNew.jsx
     // addNewFunction("Genzo");
+    const handleClick = () => {
+        alert("Confirm Click");
+    }
+
+    const handleOnChange = (value) => {
+        console.log(`OnChange ${value}`);
+
+    }
     return (
         <div className="todo-form">
-            <input type="text" className="todo-input" placeholder="Enter your task" />
-            <button className="todo-btn">Add</button>
+            {/* Hàm có tham số */}
+            <input type="text" className="todo-input" placeholder="Enter your task" onChange={(event) => { handleOnChange(event.target.value) }} />
+            {/* Hàm không có tham số */}
+            <button className="todo-btn" onClick={handleClick}>Add</button>
         </div>
     )
 }
