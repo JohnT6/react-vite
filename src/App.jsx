7c267a21a6c51bc8bd3f4c8b5651c1adb8485d9a
John Tran <phuocthanh03062004@gmail.com>
2026-02-27 17:30:37 +0700

    Thêm vào lấy số ngẫu nhiên, thêm vào id ko cho số cố định

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,11 @@ function App() {
     age: 22
   }
 
+  // Hàm lấy số ngẫu nhiên
+  function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
   const [todoList, setTodoList] = useState([
     { id: 1, name: "learning react" },
     { id: 2, name: "Nấu cơm" }
@@ -20,10 +25,11 @@ function App() {
 
   // truyền Function từ cha sang con
   // function cha đc gọi nhận giá trị mới và thêm vào todoList và gửi lại cho TodoData
+  // Đặt id là số ngẫu nhiên từ 1 tơi 1tr
   const addNewFunction = (name) => {
     console.log("Check todoList hien tai:", todoList);
     const newTodo = {
-      id: 3,
+      id: randomIntFromInterval(1, 1000000),
       name: name
     }
     setTodoList([...todoList, newTodo]);
