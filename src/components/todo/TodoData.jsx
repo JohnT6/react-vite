85a0df0f7cec8e67ac8010a9408e66b90d6c2bc9
John Tran <phuocthanh03062004@gmail.com>
2026-02-28 17:04:49 +0700

    #66. Tạo Base Giao Diện Users, cài ant design và icon. Tạo component table và component form

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -8,7 +8,7 @@ const TodoData = (props) => {
 
     return (
         <div className="todo-data">
-            <ul>
+            <ul className="todo-list">
                 {todoList.map(item =>
                     // Sử dụng key để react biết khi thêm sửa xóa thì nó sẽ tác động lên thằng nào để nó ko render lại hết nguyên 1 mảng (Lưu ý ko sử dụng index để làm giá trị của key vì khi sửa 1 phần tử bất kỳ thì các phần tử ở dưới nó sẽ bị thay đổi index và render lại làm ảnh hưởng hiệu năng)
                     <li key={item.id} className="todo-item">
