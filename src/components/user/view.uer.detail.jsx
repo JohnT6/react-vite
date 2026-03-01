1e846fc5d4abe14249a3ab363ab867556a4852d0
John Tran <phuocthanh03062004@gmail.com>
2026-03-01 17:51:01 +0700

    #82. Hiển Thị Avatar User, thêm avatar user, custom nút upload bằng label và input file

--- src/components/user/view.uer.detail.jsx
+++ src/components/user/view.uer.detail.jsx
@@ -6,6 +6,7 @@ const ViewUserDetail = (props) => {
         <>
             <Drawer
                 title="Thông tin người dùng"
+                width={"40vw"}
                 closable={{ 'aria-label': 'Close Button' }}
                 onClose={() => {
                     setDataDetail(null)
@@ -14,10 +15,31 @@ const ViewUserDetail = (props) => {
                 open={isDetailOpen}
             >
                 {dataDetail ? <>
-                    <p>Id: {dataDetail._id}</p>
-                    <p>FullName: {dataDetail.fullName}</p>
-                    <p>Email: {dataDetail.email}</p>
-                    <p>Phone: {dataDetail.phone}</p>
+                    <div style={{ display: "flex", flexDirection: "column", gap: "15px" }}>
+                        <p>Id: {dataDetail._id}</p>
+                        <p>FullName: {dataDetail.fullName}</p>
+                        <p>Email: {dataDetail.email}</p>
+                        <p>Phone: {dataDetail.phone}</p>
+                        <p>Avatar:</p>
+                        <div >
+                            <img width={300} height={250} src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`} />
+                        </div>
+                        <div>
+                            <label htmlFor="btnUpload" style={{
+                                display: "block",
+                                width: "fit-content",
+                                marginTop: "15px",
+                                padding: "5px 10px",
+                                backgroundColor: "lightblue",
+                                borderRadius: "5px",
+                                cursor: "pointer",
+                                userSelect: "none"
+                            }}>
+                                Upload Avatar
+                            </label>
+                            <input type="file" name="" id="btnUpload" hidden />
+                        </div>
+                    </div>
                 </> : <>
                     <p>Không có dữ liệu</p>
                 </>}
