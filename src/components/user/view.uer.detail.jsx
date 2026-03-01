e2cbf524d7c7aabe53e431a24408862907410769
John Tran <phuocthanh03062004@gmail.com>
2026-03-01 19:21:31 +0700

    #83. Xử Lý Sự Kiện onChange với File

--- src/components/user/view.uer.detail.jsx
+++ src/components/user/view.uer.detail.jsx
@@ -1,7 +1,37 @@
 import { Drawer } from 'antd';
+import { useEffect, useState } from 'react';
 
 const ViewUserDetail = (props) => {
     const { isDetailOpen, setIsDetailOpen, dataDetail, setDataDetail } = props;
+
+    const [selectFile, setSelectFile] = useState();
+    const [preview, setPreview] = useState();
+
+    // useEffect(() => {
+    //     if (!selectFile) {
+    //         setPreview(undefined)
+    //         return
+    //     }
+    //     const objectUrl = URL.createObjectURL(selectFile);
+    //     setPreview(objectUrl)
+
+    //     return () => URL.revokeObjectURL(selectFile)
+    // }, [selectFile])
+
+    const handleOnChangeFile = (e) => {
+        if (!e.target.files || e.target.files.length === 0) {
+            setSelectFile(null)
+            setPreview(null)
+            return
+        }
+
+        const file = e.target.files[0];
+        if (file) {
+            setSelectFile(file)
+            setPreview(URL.createObjectURL(file))
+        }
+
+    }
     return (
         <>
             <Drawer
@@ -21,8 +51,11 @@ const ViewUserDetail = (props) => {
                         <p>Email: {dataDetail.email}</p>
                         <p>Phone: {dataDetail.phone}</p>
                         <p>Avatar:</p>
-                        <div >
-                            <img width={300} height={250} src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`} />
+                        <div style={{
+                            width: "150px", height: "100px",
+                            border: "1px solid #ccc"
+                        }}>
+                            <img style={{ width: "100%", height: "100%", objectFit: "contain" }} src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`} />
                         </div>
                         <div>
                             <label htmlFor="btnUpload" style={{
@@ -37,7 +70,16 @@ const ViewUserDetail = (props) => {
                             }}>
                                 Upload Avatar
                             </label>
-                            <input type="file" name="" id="btnUpload" hidden />
+                            <input
+                                type="file" name="" id="btnUpload" hidden
+                                onChange={handleOnChangeFile}
+                            />
+                            {selectFile && <div style={{
+                                width: "150px", height: "100px",
+                                border: "1px solid #ccc"
+                            }}>
+                                <img style={{ width: "100%", height: "100%", objectFit: "contain" }} src={preview} />
+                            </div>}
                         </div>
                     </div>
                 </> : <>
