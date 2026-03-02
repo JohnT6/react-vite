3739421e94a69e255ca381912c8882d68c6e6656
John Tran <phuocthanh03062004@gmail.com>
2026-03-02 20:03:53 +0700

    #90. Hoàn thiện tính năng Register, viét api để thực hiện đăng ký gửi về backend, viết lại trang register sửa dụng form của antd

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,6 +1,80 @@
+import { Button, Form, Input, notification } from "antd";
+import { registerUserAPI } from "../services/api.service";
+import { useNavigate } from "react-router-dom";
+
 const RegisterPage = () => {
+    const [form] = Form.useForm();
+    let navigate = useNavigate();
+
+    const onFinish = async (values) => {
+        console.log(">>check values", values);
+        const res = await registerUserAPI(values.fullName, values.email, values.password, values.phone);
+        if (res.data) {
+            notification.success({
+                message: "Register success",
+                description: "Đăng ký người dùng thành công"
+            })
+            navigate("/login")
+        } else {
+            notification.error({
+                message: "Error register user",
+                description: JSON.stringify(res.message)
+            })
+        }
+    }
     return (
-        <div>Register Page</div>
+        <Form
+            form={form}
+            layout="vertical"
+            onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+
+
+        >
+
+            <div style={{ margin: "50px" }}>
+                <Form.Item
+                    label="Full Name"
+                    name="fullName"
+                    rules={[{ required: true, message: 'Please input your Full Name!' }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Email"
+                    name="email"
+                    rules={[{ required: true, message: 'Please input your Email!' }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[{ required: true, message: 'Please input your Password!' }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Phone Number"
+                    name="phone"
+                    rules={[{
+                        // required: true,
+                        pattern: new RegExp(/\d+/g),
+                        message: "Wrong format!"
+
+                    }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <div>
+                    <Button onClick={() => form.submit()} type="primary">Register</Button>
+                    <Button onClick={() => form.setFieldValue()} type="primary">test</Button>
+                </div>
+            </div>
+
+        </Form >
+
     )
 }
 
