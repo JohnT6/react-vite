f5689b95bed7723a861ace7cf396de7e4fb8e950
John Tran <phuocthanh03062004@gmail.com>
2026-03-02 20:32:06 +0700

    #91. Chia Layout Responsive (Extra), dùng grid của antd ( có 24 cột ), cách sử bootstrap

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,4 +1,4 @@
-import { Button, Form, Input, notification } from "antd";
+import { Button, Col, Form, Input, notification, Row } from "antd";
 import { registerUserAPI } from "../services/api.service";
 import { useNavigate } from "react-router-dom";
 
@@ -27,51 +27,73 @@ const RegisterPage = () => {
             form={form}
             layout="vertical"
             onFinish={onFinish}
+            style={{ margin: "10px" }}
         // onFinishFailed={onFinishFailed}
 
 
         >
 
-            <div style={{ margin: "50px" }}>
-                <Form.Item
-                    label="Full Name"
-                    name="fullName"
-                    rules={[{ required: true, message: 'Please input your Full Name!' }]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    label="Email"
-                    name="email"
-                    rules={[{ required: true, message: 'Please input your Email!' }]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[{ required: true, message: 'Please input your Password!' }]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    label="Phone Number"
-                    name="phone"
-                    rules={[{
-                        // required: true,
-                        pattern: new RegExp(/\d+/g),
-                        message: "Wrong format!"
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Full Name"
+                        name="fullName"
+                        rules={[{ required: true, message: 'Please input your Full Name!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[{ required: true, message: 'Please input your Email!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
 
-                    }]}
-                >
-                    <Input />
-                </Form.Item>
+            </Row>
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your Password!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
 
-                <div>
-                    <Button onClick={() => form.submit()} type="primary">Register</Button>
-                    <Button onClick={() => form.setFieldValue()} type="primary">test</Button>
-                </div>
-            </div>
+
+            </Row>
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Phone Number"
+                        name="phone"
+                        rules={[{
+                            // required: true,
+                            pattern: new RegExp(/\d+/g),
+                            message: "Wrong format!"
+
+                        }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+
+
+            </Row>
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <div>
+                        <Button onClick={() => form.submit()} type="primary">Register</Button>
+                    </div>
+                </Col>
+            </Row>
 
         </Form >
 
